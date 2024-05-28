41cbacc31214e0c76b6619c993ccb6b09f6e7689
arikru <krumbach.arian@gmail.com>
2024-05-28 20:39:25 +0200

    small refactor

--- src/extract.py
+++ src/extract.py
@@ -1,5 +1,38 @@
 import re
 
+from textnode import (
+        text_type_text = "text"
+        text_type_bold = "bold"
+        text_type_italic = "italic"
+        text_type_code = "code"
+        text_type_link = "link"
+        text_type_image = "image"
+)
+
+
+def split_nodes_delimiter(old_nodes, delimiter, new_text_type):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type == "text":
+            split_parts = node.text.split(delimiter)
+            
+            # Check for matching delimiters
+            if len(split_parts) > 0 and len(split_parts) % 2 == 0:
+                raise ValueError(f"Missing closing delimiter for {delimiter} in text: {node.text}")
+            
+            # Process and create new TextNodes
+            for i, part in enumerate(split_parts):
+                if i % 2 == 0:
+                    if part:
+                        new_nodes.append(TextNode(part, "text"))
+                else:
+                    new_nodes.append(TextNode(part, new_text_type))
+        else:
+            new_nodes.append(node)
+    
+    return new_nodes
+
 def extract_markdown_images(text):
     # Regex pattern to match markdown images
     pattern = r"!\[(.*?)\]\((.*?)\)"
